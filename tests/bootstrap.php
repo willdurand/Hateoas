d9aac43a85424dba5ee939b3cbbddf9d2862fe66
Adrien Brault <adrien.brault@gmail.com>
2013-07-03 00:12:43 +0200

    Remove useless require

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,5 +4,3 @@ $loader = require_once __DIR__.'/../vendor/autoload.php';
 $loader->add('tests', __DIR__.'/../');
 
 \Doctrine\Common\Annotations\AnnotationRegistry::registerLoader('class_exists');
-
-require __DIR__ . '/TestCase.php';
