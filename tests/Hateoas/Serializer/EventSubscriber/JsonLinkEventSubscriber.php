6980fd8f79d8db3cbffd7fbc90fb30e7b2215608
Adrien Brault <adrien.brault@gmail.com>
2013-07-05 20:50:39 +0200

    Share link event subscriber tests

--- tests/Hateoas/Serializer/EventSubscriber/JsonLinkEventSubscriber.php
+++ tests/Hateoas/Serializer/EventSubscriber/JsonLinkEventSubscriber.php
@@ -2,51 +2,22 @@
 
 namespace tests\Hateoas\Serializer\EventSubscriber;
 
-use tests\TestCase;
 use Hateoas\Serializer\EventSubscriber\JsonLinkEventSubscriber as TestedJsonLinkEventSubscriber;
 
-class JsonLinkEventSubscriber extends TestCase
+class JsonLinkEventSubscriber extends AbstractLinkEventSubscriberTest
 {
-    public function testOnPostSerialize()
+    protected function createLinkEventSubscriber($linksFactory, $serializer)
     {
-        $this->mockGenerator->orphanize('__construct');
+        return new TestedJsonLinkEventSubscriber($linksFactory, $serializer);
+    }
 
-        $links = array(
-            new \mock\Hateoas\Model\Link(),
-        );
-        $object = new \StdClass();
+    protected function createSerializerMock()
+    {
+        return new \mock\Hateoas\Serializer\JsonSerializerInterface();
+    }
 
-        $this->mockGenerator->orphanize('__construct');
-        $linksFactory = new \mock\Hateoas\Factory\LinksFactory();
-        $linksFactory->getMockController()->createLinks = function () use ($links) {
-            return $links;
-        };
-        $jsonSerializer = new \mock\Hateoas\Serializer\JsonSerializerInterface();
-
-        $this->mockGenerator->orphanize('__construct');
-        $jsonSerializationVisitor = new \mock\JMS\Serializer\JsonSerializationVisitor();
-
-        $this->mockGenerator->orphanize('__construct');
-        $event = new \mock\JMS\Serializer\EventDispatcher\ObjectEvent();
-        $event->getMockController()->getObject = function () use ($object) {
-            return $object;
-        };
-        $event->getMockController()->getVisitor = function () use ($jsonSerializationVisitor) {
-            return $jsonSerializationVisitor;
-        };
-
-        $jsonLinkEventSubscriber = new TestedJsonLinkEventSubscriber($linksFactory, $jsonSerializer);
-        $jsonLinkEventSubscriber->onPostSerialize($event);
-
-        $this
-            ->mock($linksFactory)
-                ->call('createLinks')
-                    ->withArguments($object)
-                    ->once()
-            ->mock($jsonSerializer)
-                ->call('serializeLinks')
-                    ->withArguments($links, $jsonSerializationVisitor)
-                    ->once()
-        ;
+    protected function createSerializationVisitorMock()
+    {
+        return new \mock\JMS\Serializer\JsonSerializationVisitor();
     }
 }
