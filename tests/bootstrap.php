270eb26858e043b279287d3e570ce807130dffee
William DURAND <william.durand1@gmail.com>
2013-10-18 10:41:51 +0200

    Fix tests setup

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,17 @@
 <?php
 
+if (!($loader = include __DIR__ . '/../vendor/autoload.php')) {
+    die(<<<EOT
+You need to install the project dependencies using Composer:
+$ wget http://getcomposer.org/composer.phar
+OR
+$ curl -s https://getcomposer.org/installer | php
+$ php composer.phar install --dev
+$ phpunit
+EOT
+    );
+}
+
+$loader->add('Hateoas\Tests', __DIR__);
+
 \Doctrine\Common\Annotations\AnnotationRegistry::registerLoader('class_exists');
