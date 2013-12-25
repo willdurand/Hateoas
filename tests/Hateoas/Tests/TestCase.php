d3aa41c7dc784c895099bd2a6b3147e28037ff28
William DURAND <william.durand1@gmail.com>
2013-12-25 19:45:53 +0100

    fix tests for PHP 5.3

--- tests/Hateoas/Tests/TestCase.php
+++ tests/Hateoas/Tests/TestCase.php
@@ -13,6 +13,10 @@ abstract class TestCase extends BaseTestCase
 
     protected function json($string)
     {
+        if (version_compare(PHP_VERSION, '5.4.0', '<')) {
+            throw new \PHPUnit_Framework_IncompleteTestError('This test requires PHP 5.4+');
+        }
+
         return $this->string(
             json_encode(json_decode($string), JSON_PRETTY_PRINT)
         );
