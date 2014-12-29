14c5d8b913c8e2ada4b4f33d9f86e65081d62972
Giorgio Premi <giosh94mhz@gmail.com>
2014-12-29 18:07:10 +0100

    Add OffsetRepresentation and AbstractSegmentedRepresentation

---
+++ src/Hateoas/Representation/AbstractSegmentedRepresentation.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Hateoas\Representation;
+
+use Hateoas\Configuration\Annotation as Hateoas;
+use JMS\Serializer\Annotation as Serializer;
+
+/**
+ * @Serializer\ExclusionPolicy("all")
+ *
+ * @author Premi Giorgio <giosh94mhz@gmail.com>
+ */
+abstract class AbstractSegmentedRepresentation extends RouteAwareRepresentation
+{
+    /**
+     * @var int
+     *
+     * @Serializer\Expose
+     * @Serializer\Type("integer")
+     * @Serializer\XmlAttribute
+     */
+    private $limit;
+
+    /**
+     * @var int
+     *
+     * @Serializer\Expose
+     * @Serializer\Type("integer")
+     * @Serializer\XmlAttribute
+     */
+    private $total;
+
+    /**
+     * @var string
+     */
+    private $limitParameterName;
+
+    public function __construct(
+        $inline,
+        $route,
+        array $parameters        = array(),
+        $limit,
+        $total                   = null,
+        $limitParameterName      = null,
+        $absolute                = false
+    ) {
+        parent::__construct($inline, $route, $parameters, $absolute);
+
+        $this->total               = $total;
+        $this->limit               = $limit;
+        $this->limitParameterName  = $limitParameterName ?: 'limit';
+    }
+
+    /**
+     * @return int
+     */
+    public function getLimit()
+    {
+        return $this->limit;
+    }
+
+    /**
+     * @param  null  $offset
+     * @param  null  $limit
+     * @return array
+     */
+    public function getParameters($limit = null)
+    {
+        $parameters = parent::getParameters();
+
+        $parameters[$this->limitParameterName] = null === $limit ? $this->getLimit() : $limit;
+
+        return $parameters;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTotal()
+    {
+        return $this->total;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLimitParameterName()
+    {
+        return $this->limitParameterName;
+    }
+
+    /**
+     * @param string $paramName
+     */
+    protected function moveParameterToEnd(array &$parameters, $key)
+    {
+        if (! array_key_exists($key, $parameters)) {
+            return;
+        }
+
+        $value = $parameters[$key];
+        unset($parameters[$key]);
+        $parameters[$key] = $value;
+    }
+}
---
+++ src/Hateoas/Representation/OffsetRepresentation.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Hateoas\Representation;
+
+use Hateoas\Configuration\Annotation as Hateoas;
+use JMS\Serializer\Annotation as Serializer;
+
+/**
+ * @Serializer\ExclusionPolicy("all")
+ * @Serializer\XmlRoot("collection")
+ * @Serializer\AccessorOrder("custom", custom = {"offset", "limit", "total"})
+ *
+ * @Hateoas\Relation(
+ *      "first",
+ *      href = @Hateoas\Route(
+ *          "expr(object.getRoute())",
+ *          parameters = "expr(object.getParameters(0))",
+ *          absolute = "expr(object.isAbsolute())"
+ *      )
+ * )
+ * @Hateoas\Relation(
+ *      "last",
+ *      href = @Hateoas\Route(
+ *          "expr(object.getRoute())",
+ *          parameters = "expr(object.getParameters((object.getTotal() - 1) - (object.getTotal() - 1) % object.getLimit()))",
+ *          absolute = "expr(object.isAbsolute())"
+ *      ),
+ *      exclusion = @Hateoas\Exclusion(
+ *          excludeIf = "expr(object.getTotal() === null)"
+ *      )
+ * )
+ * @Hateoas\Relation(
+ *      "next",
+ *      href = @Hateoas\Route(
+ *          "expr(object.getRoute())",
+ *          parameters = "expr(object.getParameters(object.getOffset() + object.getLimit()))",
+ *          absolute = "expr(object.isAbsolute())"
+ *      ),
+ *      exclusion = @Hateoas\Exclusion(
+ *          excludeIf = "expr(object.getTotal() !== null && (object.getOffset() + object.getLimit()) >= object.getTotal())"
+ *      )
+ * )
+ * @Hateoas\Relation(
+ *      "previous",
+ *      href = @Hateoas\Route(
+ *          "expr(object.getRoute())",
+ *          parameters = "expr(object.getParameters((object.getOffset() > object.getLimit()) ? object.getOffset() - object.getLimit() : 0))",
+ *          absolute = "expr(object.isAbsolute())"
+ *      ),
+ *      exclusion = @Hateoas\Exclusion(
+ *          excludeIf = "expr(! object.getOffset())"
+ *      )
+ * )
+ *
+ * @author Premi Giorgio <giosh94mhz@gmail.com>
+ */
+class OffsetRepresentation extends AbstractSegmentedRepresentation
+{
+    /**
+     * @var int
+     *
+     * @Serializer\Expose
+     * @Serializer\XmlAttribute
+     */
+    private $offset;
+
+    /**
+     * @var string
+     */
+    private $offsetParameterName;
+
+    public function __construct(
+        $inline,
+        $route,
+        array $parameters        = array(),
+        $offset,
+        $limit,
+        $total                   = null,
+        $offsetParameterName     = null,
+        $limitParameterName      = null,
+        $absolute                = false
+    ) {
+        parent::__construct($inline, $route, $parameters, $limit, $total, $limitParameterName, $absolute);
+
+        $this->offset              = $offset;
+        $this->offsetParameterName = $offsetParameterName  ?: 'offset';
+    }
+
+    /**
+     * @return int
+     */
+    public function getOffset()
+    {
+        return $this->offset;
+    }
+
+    /**
+     * @param  null  $offset
+     * @param  null  $limit
+     * @return array
+     */
+    public function getParameters($offset = null, $limit = null)
+    {
+        $parameters = parent::getParameters($limit);
+
+        unset($parameters[$this->offsetParameterName]);
+
+        if (null === $offset) {
+            $offset = $this->getOffset();
+        }
+
+        if ($offset) {
+            $parameters[$this->offsetParameterName] = $offset;
+            $this->moveParameterToEnd($parameters, $this->getLimitParameterName());
+        }
+
+        return $parameters;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOffsetParameterName()
+    {
+        return $this->offsetParameterName;
+    }
+}
