c8fbfe6075eee343890e0bf63beca27dfad85b77
ixarlie <ixarlie@gmail.com>
2023-02-01 00:33:44 +0100

    fix tests. use new user class for php attributes

--- tests/Hateoas/Tests/Configuration/Metadata/Driver/AttributeDriverTest.php
+++ tests/Hateoas/Tests/Configuration/Metadata/Driver/AttributeDriverTest.php
@@ -10,11 +10,18 @@ use Hateoas\Configuration\Metadata\Driver\AttributeDriver\AttributeReader;
 
 class AttributeDriverTest extends AbstractDriverTest
 {
-    public function setUp(): void
+    protected function getUserClass(): string
+    {
+        return 'Hateoas\Tests\Fixtures\UserPhpAttributes';
+    }
+
+    public function testUser()
     {
         if (PHP_VERSION_ID < 80100) {
             $this->markTestSkipped('Attributes are available only on php 8.1 or higher');
         }
+
+        parent::testUser();
     }
 
     public function createDriver()
